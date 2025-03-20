41f00916e6a05623f698ae99bed5eb8882a162a3
Dustin-08 <cjhjik1@gmail.com>
2025-03-20 12:30:09 +0900

    3번 문제 해결 및 cpp 파일로 변경

---
+++ 03_반복문/step03_no.03_8393.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace::std;
+
+int n, sum;
+
+int main() {
+    cin >> n;
+    for(int i = 1; i <= n; ++i){
+        sum = sum + i;
+    }
+    cout << sum;
+    return 0;
+}
