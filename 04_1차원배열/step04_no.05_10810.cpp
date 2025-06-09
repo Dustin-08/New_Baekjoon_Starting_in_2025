fd5b0bec327e6d545a5eb61961aa4516eae21f3c
Dustin-08 <cjhjik1@gmail.com>
2025-06-09 21:43:41 +0900

    Update step04_no.05_10810.cpp
    문제 풀 예정

--- 04_1차원배열/step04_no.05_10810.cpp
+++ 04_1차원배열/step04_no.05_10810.cpp
@@ -2,21 +2,6 @@
 
 using namespace::std;
 
-int arr[10];
-int maxValue;
-
 int main() {
-    for (int i = 0; i < 9; ++i) {
-        cin >> arr[i];
-    }
-    for (int i = 0; i < 9; ++i) {
-        maxValue = max(maxValue, arr[i]);
-    }
-    cout << maxValue << "\n";
-    for (int i = 0; i < 9; ++i) {
-        if (maxValue == arr[i]) {
-            cout << i + 1;
-        }
-    }
     return 0;
 }
