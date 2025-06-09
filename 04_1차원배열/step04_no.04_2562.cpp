57f51e971bfb61ef222f3e7dd5303ca72cc21077
Dustin-08 <cjhjik1@gmail.com>
2025-06-09 21:43:22 +0900

    4번 문제 풀이 및 cpp 파일로 변경

--- 04_1차원배열/step04_no.04_2562.cpp
+++ 04_1차원배열/step04_no.04_2562.cpp
@@ -2,6 +2,21 @@
 
 using namespace::std;
 
+int arr[10];
+int maxValue;
+
 int main() {
+    for (int i = 0; i < 9; ++i) {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < 9; ++i) {
+        maxValue = max(maxValue, arr[i]);
+    }
+    cout << maxValue << "\n";
+    for (int i = 0; i < 9; ++i) {
+        if (maxValue == arr[i]) {
+            cout << i + 1;
+        }
+    }
     return 0;
 }
