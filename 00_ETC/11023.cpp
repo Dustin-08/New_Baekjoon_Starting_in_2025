4e62ae3b9671658320251bd5fe9e8aa172dc65b2
Dustin-08 <cjhjik1@gmail.com>
2025-06-14 19:57:21 +0900

    Create 11023.cpp

---
+++ 00_ETC/11023.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace::std;
+
+int n, sum = 0;
+
+int main() {
+    while (cin >> n) {
+        sum += n;
+    }
+    cout << sum;
+}
+//
+// Created by 최승우 on 25. 6. 14.
+//
