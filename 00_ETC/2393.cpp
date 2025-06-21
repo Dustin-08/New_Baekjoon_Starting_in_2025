658dbe038c5ad77d3b65288a472c9b267e424fe2
Dustin-08 <cjhjik1@gmail.com>
2025-06-21 21:24:16 +0900

    Create 2393.cpp

---
+++ 00_ETC/2393.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "  ___  ___  ___\n";
+    cout << "  | |__| |__| |\n";
+    cout << "  |           |\n";
+    cout << "   \\_________/\n";
+    cout << "    \\_______/\n";
+    cout << "     |     |\n";
+    cout << "     |     |\n";
+    cout << "     |     |\n";
+    cout << "     |     |\n";
+    cout << "     |_____|\n";
+    cout << "  __/       \\__\n";
+    cout << " /             \\\n";
+    cout << "/_______________\\\n";
+
+    return 0;
+}
+
+//
+// Created by 최승우 on 25. 6. 21.
+//
