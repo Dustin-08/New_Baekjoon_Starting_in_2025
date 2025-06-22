5fcb37db064f21470a2f98fd5af6c5cb450f7d2e
Dustin-08 <cjhjik1@gmail.com>
2025-06-23 07:00:29 +0900

    Create 2741.cpp

---
+++ 00_ETC/2741.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int n;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cout << i + 1 << "\n";
+    }
+    return 0;
+}
+
+//
+// Created by 최승우 on 25. 6. 23.
+//
