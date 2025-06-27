143bb9255bdb1f61118c2e357a1ef0fac538fc44
Dustin-08 <cjhjik1@gmail.com>
2025-06-27 15:04:21 +0900

    Create 5337.cpp

---
+++ 00_ETC/5337.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout << ".  .   ." << "\n";
+    cout << "|  | _ | _. _ ._ _  _" << "\n";
+    cout << "|/\\|(/.|(_.(_)[ | )(/.";
+    return 0;
+}
+
+//
+// Created by 최승우 on 25. 6. 27.
+//
