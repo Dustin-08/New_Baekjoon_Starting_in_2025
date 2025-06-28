233227127e880925b9221336181553e9fd70124e
Dustin-08 <cjhjik1@gmail.com>
2025-06-28 14:18:40 +0900

    Create 24723.cpp

---
+++ 00_ETC/24723.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int n;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n;
+
+    cout << pow(2, n);
+    return 0;
+}
+
+//
+// Created by 최승우 on 25. 6. 28.
+//
