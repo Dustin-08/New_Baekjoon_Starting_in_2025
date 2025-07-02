6fca980b45d104fba281cfed4350c7142c91f709
Dustin-08 <cjhjik1@gmail.com>
2025-07-02 19:12:35 +0900

    Create 32384.cpp

---
+++ 00_ETC/32384.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int n;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cout << "LoveisKoreaUniversity ";
+    }
+    return 0;
+}
+//
+// Created by 최승우 on 25. 7. 2.
+//
