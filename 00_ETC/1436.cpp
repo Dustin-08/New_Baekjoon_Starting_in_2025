55d48b2646987a27ccc170f1af1a815e43e68a7b
Dustin-08 <cjhjik1@gmail.com>
2025-07-03 20:06:17 +0900

    Create 1436.cpp

---
+++ 00_ETC/1436.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int n;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    int number = 666;
+    int cnt = 0;
+    while (true) {
+        if (to_string(number).find("666") != string::npos) {
+            // "666" 포함 여부 확인(최소 6이 3개 있어야함)
+            ++cnt; // 종말의 수 찾으면 cnt 증가
+            if (cnt == n) {
+                cout << number;
+                break;
+            }
+        }
+        ++number;
+    }
+
+
+    return 0;
+}
+//
+// Created by 최승우 on 25. 7. 3.
+//
