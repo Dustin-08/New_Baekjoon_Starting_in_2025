01127dc0b66a2c9c3dc3fc239e7e4fd5d1a2fd89
Dustin-08 <cjhjik1@gmail.com>
2025-07-03 20:10:31 +0900

    Create 2751.cpp

---
+++ 00_ETC/2751.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n;
+vector<int> v;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    v.resize(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    for (int i = 0; i < n; ++i) {
+        cout << v[i] << "\n";
+    }
+
+    return 0;
+}
+//
+// Created by 최승우 on 25. 7. 3.
+//
