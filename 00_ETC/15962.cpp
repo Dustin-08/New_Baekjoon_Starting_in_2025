b2050b86ac2af34518be7b3ab9b99ce5e773abf2
Dustin-08 <cjhjik1@gmail.com>
2025-07-25 22:16:05 +0900

    Create 15962.cpp

---
+++ 00_ETC/15962.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout << "파이팅!!";
+    return 0;
+}
+//
+// Created by 최승우 on 25. 7. 25.
+//
