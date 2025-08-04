fce2b9cad9bc427e63b5da95458dd71ba893a6fb
Dustin-08 <cjhjik1@gmail.com>
2025-08-04 15:03:48 +0900

    Create 29790.cpp

---
+++ 00_ETC/29790.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, u, l;
+    cin >> n >> u >> l;
+
+    if (n >= 1000) {
+        if (u >= 8000 || l >= 260) {
+            cout << "Very Good";
+        } else {
+            cout << "Good";
+        }
+    } else {
+        cout << "Bad";
+    }
+    return 0;
+}
+//
+// Created by 최승우 on 25. 8. 4.
+//
