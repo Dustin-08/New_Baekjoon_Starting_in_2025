4666f75c8078c7077e00e091488411969a4bbd26
Dustin-08 <cjhjik1@gmail.com>
2025-08-15 14:18:45 +0900

    Create 10867.cpp

---
+++ 00_ETC/10867.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
+    for (int i = 0; i < v.size(); ++i) {
+        cout << v[i] << " ";
+    }
+    return 0;
+}
+//
+// Created by 최승우 on 25. 8. 15.
+//
