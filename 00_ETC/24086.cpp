991d970218123f53c02e5b2b0ab724e81812f3b6
Dustin-08 <cjhjik1@gmail.com>
2025-09-08 20:33:58 +0900

    Create 24086.cpp

---
+++ 00_ETC/24086.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int a, b;
+    cin >> a;
+    cin >> b;
+    cout << b - a << '\n';
+    return 0;
+}
+//
+// Created by 최승우 on 25. 9. 6.
+//
