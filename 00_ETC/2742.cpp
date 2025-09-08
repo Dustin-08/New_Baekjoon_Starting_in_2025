d5ce0a82ace4dcf74d3c1ce9d2f635f48726b259
Dustin-08 <cjhjik1@gmail.com>
2025-09-08 20:34:00 +0900

    Create 2742.cpp

---
+++ 00_ETC/2742.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n;
+    cin >> n;
+    for (int i = n; i >= 1; --i) {
+        cout << i << '\n';
+    }
+    return 0;
+}
+//
+// Created by 최승우 on 25. 9. 3.
+//
