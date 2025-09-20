a4d2926fd20104746f774cf69c13fbb828ad9fb1
Dustin-08 <cjhjik1@gmail.com>
2025-09-20 22:34:38 +0900

    Create 17826.py

---
+++ 00_ETC_PY/17826.py
@@ -0,0 +1,19 @@
+a = list(map(int, input().split()))
+n = int(input())
+
+rank = a.index(n) + 1
+
+if 1 <= rank <= 5:
+    print("A+")
+elif 6 <= rank <= 15:
+    print("A0")
+elif 16 <= rank <= 30:
+    print("B+")
+elif 31 <= rank <= 35:
+    print("B0")
+elif 36 <= rank <= 45:
+    print("C+")
+elif 46 <= rank <= 48:
+    print("C0")
+elif 49 <= rank <= 50:
+    print("F")
