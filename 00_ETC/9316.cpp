34964ef6b838af5af81221cbdbb7235fd7357eba
Dustin-08 <cjhjik1@gmail.com>
2025-09-20 22:34:33 +0900

    Create 9316.cpp

---
+++ 00_ETC/9316.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i) {
+        cout << "Hello World, Judge " << i << "!" << "\n";
+    }
+    return 0;
+}
+//
+// Created by 최승우 on 25. 9. 17.
+//
