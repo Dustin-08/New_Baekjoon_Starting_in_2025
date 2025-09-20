b3e3f18970f503e57dd1507e192464e18bc95c19
Dustin-08 <cjhjik1@gmail.com>
2025-09-20 22:34:30 +0900

    Create 28235.cpp

---
+++ 00_ETC/28235.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    string s;
+    cin >> s;
+    if (s == "SONGDO") {
+        cout << "HIGHSCHOOL";
+    } else if (s == "CODE") {
+        cout << "MASTER";
+    } else if (s == "2023") {
+        cout << "0611";
+    } else if (s == "ALGORITHM") {
+        cout << "CONTEST";
+    }
+    return 0;
+}
+//
+// Created by 최승우 on 25. 9. 18.
+//
