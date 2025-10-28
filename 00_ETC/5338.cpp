110d07bd9db2088ed6dce49a9ea6011633765a8f
Dustin-08 <cjhjik1@gmail.com>
2025-10-28 10:00:07 +0900

    Create 5338.cpp

---
+++ 00_ETC/5338.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    cout << "       _.-;;-._" << "\n";
+    cout << "'-..-'|   ||   |" << "\n";
+    cout << "'-..-'|_.-;;-._|" << "\n";
+    cout << "'-..-'|   ||   |" << "\n";
+    cout << "'-..-'|_.-''-._|" << "\n";
+
+    return 0;
+}
+//
+// Created by 최승우 on 25. 10. 28.
+//
